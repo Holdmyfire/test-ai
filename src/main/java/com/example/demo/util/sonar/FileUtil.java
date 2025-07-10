31054ffa7ad353f2a3513fc04e2a6a9d4ed18b55
Shawn <Holdmyfire@users.noreply.github.com>
2025-07-10 19:49:21 +0800

    Update FileUtil.java

--- src/main/java/com/example/demo/util/sonar/FileUtil.java
+++ src/main/java/com/example/demo/util/sonar/FileUtil.java
@@ -163,4 +163,37 @@ public class FileUtil {
         }
         return flag;
     }
+    /**
+     * 只删除文件夹里面内容
+     */
+    public static boolean delAllFile4(String path) {
+        boolean flag = false;
+        File file = new File(path);
+        if (!file.exists()) {
+            return flag;
+        }
+        if (!file.isDirectory()) {
+            return flag;
+        }
+        String[] tempList = file.list();
+        File temp = null;
+        for (int i = 0; i < tempList.length; i++) {
+            if (path.endsWith(File.separator)) {
+                temp = new File(path + tempList[i]);
+            } else {
+                temp = new File(path + File.separator + tempList[i]);
+            }
+            if (temp.isFile()) {
+                temp.delete();
+            }
+            if (temp.isDirectory()) {
+                //先删除文件夹里面的文件
+                delAllFile(path + "/" + tempList[i]);
+                //再删除空文件夹
+                delFolder(path + "/" + tempList[i]);
+                flag = true;
+            }
+        }
+        return flag;
+    }
 }
